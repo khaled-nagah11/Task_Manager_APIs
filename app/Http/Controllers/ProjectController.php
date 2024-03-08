be5b26a91fd39f51746fb2e06bac7c1eea21ef9d
khaled <kaled21@std.mans.edu.eg>
2024-03-08 22:20:11 +0200

    paginate projects and tasks

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,16 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\updateProjectRequest;
+use App\Http\Resources\ProjectCollection;
 use App\Http\Resources\ProjectResource;
 use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class ProjectController extends Controller
 {
+    public function index()
+    {
+        $projects = QueryBuilder::for(Project::class)
+            ->allowedIncludes('tasks')
+            ->paginate();
+        return new ProjectCollection($projects);
+    }
     public function show(Request $request , Project $project)
     {
-        return new ProjectResource($project);
+        return (new ProjectResource($project))->load('tasks');
     }
 
     public function store(StoreProjectRequest $request)
